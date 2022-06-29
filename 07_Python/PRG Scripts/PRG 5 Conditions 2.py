f42bca43356a39c3f289d6c6ce95e9cc893db467
abdeslamazhir <104759236+abdeslamazhir@users.noreply.github.com>
2022-06-29 17:06:24 +0200

    b

--- 07_Python/PRG Scripts/PRG 5 Conditions 2.py
+++ 07_Python/PRG Scripts/PRG 5 Conditions 2.py
@@ -2,10 +2,10 @@ number = int (input("Enter a number:"))
 while number != 100:
     if number >100:
         print(number, "this number is to high")
-        number = int(input("Enter a number:"))
+        number = int(input("Enter a number:"))       # If the number is lower or higher dan 100 you will get the question "Enter a number:"
     elif number < 100:
         print(number, "This number is to low")
         number = int(input("Enter a number:"))
 else:
-    if number == 100:
+    if number == 100:                                 # The loop will stop when the number 100 is entered.
         print(number , "This is 100% good!")
