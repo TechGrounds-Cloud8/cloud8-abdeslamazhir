91d2bb4663a897e5c17e50410129d238ce802f75
abdeslamazhir <104759236+abdeslamazhir@users.noreply.github.com>
2022-07-01 15:30:07 +0200

    q

---
+++ 07_Python/PRG 9 Mini Project: Number Guessing.md
@@ -0,0 +1,27 @@
+# Mini Project: Number Guessing
+
+
+
+
+
+## Exercise
+
+
+
+- Generate a random number between 1 and 100 (or any other range).
+- The player guesses a number. For every wrong answer the player receives a clue.
+- When the player guesses the right number, display a score.
+
+
+
+
+
+
+
+
+### Results
+
+
+[code]()
+![screenshot](../00_includes/python/game.png)
+
--- 07_Python/PRG Scripts/PRG 7 lists 2.py
+++ 07_Python/PRG Scripts/PRG 7 lists 2.py
@@ -1,15 +1,17 @@
 # A list with 5 numbers.
-list = [10, 20, 30, 40, 50]  
+list = [10, 20, 30, 40, 50, 60]  
+      
 
   # Print the value of that item added to the value of the next item in the list. 
   # If it is the last item,add it to the value of the first item instead.
   
 print ("My numbers:", list)                           
 for i in range(len(list)):
-    if i == len(list)-1:
-              print(list[i] + list[0])
-    else:
-        print(list[i] + list[i+1])
+  print(i) 
+  if i == len(list) -1:
+    print(list[i] + list[0])
+  else:
+    print(list[i] + list[i+1])
 
 
 
