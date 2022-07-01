600d4d551c2693a893648ca28987e1207d071c44
abdeslamazhir <104759236+abdeslamazhir@users.noreply.github.com>
2022-07-01 16:08:57 +0200

    g

--- 07_Python/PRG Scripts/PRG 9 Mini Project: Number Guessing.py
+++ 07_Python/PRG Scripts/PRG 9 Mini Project: Number Guessing.py
@@ -1,23 +1,21 @@
 import random
-
 number = random.randint(1, 10)       # Generate random number
 num_of_guesses = 0                   # Number of guesses player took
 guess = 0                            # Player's current guess
-
 # Introduce player to game
 print('Welcome to the number guessing game!')
 print('I have come up with a number between 1 and 10, guess what it is.')
-
 running = True
 while running:
-  
   guess = int(input("What is the number?:"))
   num_of_guesses += 1
-  
+
   if guess > number:
     print('Too high, pls try again.')
+
   elif guess < number:
     print('Too low, pls try again.')
+
   else:
     print('Good job!')
     print(f'It only took you {num_of_guesses} tries')
